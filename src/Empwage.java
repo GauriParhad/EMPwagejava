f7eab22f0fb55071ba9dcfa33d405b1f096cedf3
Gaurip10 <thegauriparhad@gmail.com>
2021-08-06 10:04:26 +0530

    Emp is parttime or full time

--- src/Empwage.java
+++ src/Empwage.java
@@ -4,17 +4,24 @@ public class Empwage {
         System.out.println("Welcome to Employee wage computation program. ");
 
         //constant
-        int IS_PRESENT = 1;
+        int IS_FULL_TIME = 2;
+        int IS_PART_TIME = 1;
         int EMP_RATE_PER_HR = 20;
-        int Emphrs=0;
-        int Empwage=0;
+        int Emphrs = 0;
+        int Empwage = 0;
 
         int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_PRESENT)
-            Emphrs=8;
-
-        else
-            Emphrs=0;
+        if (empCheck == IS_FULL_TIME) {
+            System.out.println("Employee is full time present.");
+            Emphrs = 8;
+        }
+        if (empCheck == IS_PART_TIME) {
+            System.out.println("Employee is part time present.");
+            Emphrs = 4;
+        } else {
+            System.out.println("Employee is Absent.");
+            Emphrs = 0;
+        }
 
         Empwage = Emphrs *  EMP_RATE_PER_HR;
         System.out.println(" Employee wage is:"+Empwage);
