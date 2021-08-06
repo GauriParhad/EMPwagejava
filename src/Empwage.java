f0ef29972df0aa3b6b9b150ef7ec02e0db78e7b1
Gaurip10 <thegauriparhad@gmail.com>
2021-08-06 09:52:34 +0530

    Emp wage

--- src/Empwage.java
+++ src/Empwage.java
@@ -5,12 +5,19 @@ public class Empwage {
 
         //constant
         int IS_PRESENT = 1;
+        int EMP_RATE_PER_HR = 20;
+        int Emphrs=0;
+        int Empwage=0;
 
         int empCheck = (int) Math.floor(Math.random() * 10) % 2;
         if (empCheck == IS_PRESENT)
-            System.out.println(" Employee is Present");
+            Emphrs=8;
+
         else
-            System.out.println(" Employee is Absent");
+            Emphrs=0;
+
+        Empwage = Emphrs *  EMP_RATE_PER_HR;
+        System.out.println(" Employee wage is:"+Empwage);
     }
 }
 
